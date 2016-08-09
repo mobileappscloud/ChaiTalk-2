a794c1eb6d519ccef00b0f2b8ef1aee5c063d7a6
mobileappscloud <doc92606@gmail.com>
2016-08-09 12:51:16 -0700

    made a comment to test if the clone is working

--- gameofchats/ChatMessageCell.swift
+++ gameofchats/ChatMessageCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ChatMessageCell: UICollectionViewCell
 {
-    var chatLogController: ChatLogController?
+    var chatLogController: ChatLogController? //Test commit
     
     let textView: UITextView =
     {
